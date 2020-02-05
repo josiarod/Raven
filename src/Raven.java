8f6600c2179f70278e9dc899987f2986e0e4ce8f
GBTC440011UR <GBTC440011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-05 11:37:28 -0500

    initial commit

---
+++ src/Raven.java
@@ -0,0 +1,31 @@
+public class Raven {
+
+    private String input;
+
+    public Raven(){
+
+    }
+
+    public String answer(String input){
+        String [ ] positive = {"good", "glad", "happy", "relaxed", "accomplished", "alert", "creative"};
+        String [ ] negative = {"bad", "sad", "tired", "angry", "anxious", "hungry", "moody", "afraid"};
+        String [ ] inputArray = input.split(" ");
+
+        String answer = "";
+       for(String s:)
+
+
+
+        return answer;
+
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+}
